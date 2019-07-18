b7c3a16efc88738051d4b836c7bddaaa0ff321a8
Mkv-art <95780617@qq.com>
2019-07-18 10:43:10 +0800

    rename file

--- src/main/java/com/example/gittest1/Application.java
+++ src/main/java/com/example/gittest1/Application.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Gittest1Application {
+public class Application {
 
+    //修改
     public static void main(String[] args) {
-        SpringApplication.run(Gittest1Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
